473e865b3807e8a7f2a2ef2e101a56e48a27951a
tony2001 <tony2001@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-08-28 12:19:04 +0000

    update unrar to version 3.6.8
    git-svn-id: http://svn.php.net/repository/pecl/rar/trunk@218970 c90b9560-bf6c-de11-be94-00142212c4b1

--- unrar/version.hpp
+++ unrar/version.hpp
@@ -1,6 +1,6 @@
 #define RARVER_MAJOR     3
 #define RARVER_MINOR    60
-#define RARVER_BETA      6
-#define RARVER_DAY       3
-#define RARVER_MONTH     7
+#define RARVER_BETA      0
+#define RARVER_DAY       5
+#define RARVER_MONTH     8
 #define RARVER_YEAR   2006
