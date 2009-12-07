cddd4631bb753ed29ea027e03bbdfc985272dd55
cataphract <cataphract@c90b9560-bf6c-de11-be94-00142212c4b1>
2009-12-07 16:48:43 +0000

    Restored LPARAM to long and let's hope sizeof(long) == sizeof(void*) in non-windows platforms.
    git-svn-id: https://svn.php.net/repository/pecl/rar/trunk@291824 c90b9560-bf6c-de11-be94-00142212c4b1

--- unrar/version.hpp
+++ unrar/version.hpp
@@ -1,7 +1,7 @@
 #define RARVER_MAJOR     3
 #define RARVER_MINOR    90
 #define RARVER_BETA      0
-#define RARVER_PATCH     6
+#define RARVER_PATCH     7
 #define RARVER_DAY      07
 #define RARVER_MONTH    12
 #define RARVER_YEAR   2009
