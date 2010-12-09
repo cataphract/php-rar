e0e67ef1522e9c4de4be1c4d01d442e72703159a
cataphract <cataphract@c90b9560-bf6c-de11-be94-00142212c4b1>
2010-12-09 05:13:01 +0000

    - Fixed PECL bug #18449 (Extraction of uncompressed and encrypted files fails).
    git-svn-id: https://svn.php.net/repository/pecl/rar/trunk@306110 c90b9560-bf6c-de11-be94-00142212c4b1

--- unrar/version.hpp
+++ unrar/version.hpp
@@ -1,7 +1,7 @@
 #define RARVER_MAJOR     3
 #define RARVER_MINOR    93
 #define RARVER_BETA      0
-#define RARVER_PATCH     5
-#define RARVER_DAY      02
-#define RARVER_MONTH    07
+#define RARVER_PATCH     6
+#define RARVER_DAY      09
+#define RARVER_MONTH    12
 #define RARVER_YEAR   2010
