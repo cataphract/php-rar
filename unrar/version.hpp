14afaf33a94728becea75abd61fe53a01ae2b49b
Gustavo Lopes <glopes@nebm.ist.utl.pt>
2011-01-13 19:08:38 +0000

    Updated to unrar 4.0.4

--- unrar/version.hpp
+++ unrar/version.hpp
@@ -1,6 +1,7 @@
 #define RARVER_MAJOR     4
 #define RARVER_MINOR     0
-#define RARVER_BETA      2
-#define RARVER_DAY      26
-#define RARVER_MONTH    11
+#define RARVER_BETA      4
+#define RARVER_PATCH     1
+#define RARVER_DAY      13
+#define RARVER_MONTH    01
 #define RARVER_YEAR   2010
