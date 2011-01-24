ec4610e02e84fe4f3073d8e6b145249a45f6ccc2
cataphract <cataphract@c90b9560-bf6c-de11-be94-00142212c4b1>
2011-01-24 04:43:38 +0000

    Merge branch 'unrar_temp', with changes in unrar 4.0.5.
    git-svn-id: https://svn.php.net/repository/pecl/rar/trunk@307694 c90b9560-bf6c-de11-be94-00142212c4b1

--- unrar/version.hpp
+++ unrar/version.hpp
@@ -1,7 +1,7 @@
 #define RARVER_MAJOR     4
 #define RARVER_MINOR     0
-#define RARVER_BETA      4
+#define RARVER_BETA      5
 #define RARVER_PATCH     1
-#define RARVER_DAY      13
-#define RARVER_MONTH    01
+#define RARVER_DAY      24
+#define RARVER_MONTH     1
 #define RARVER_YEAR   2011
