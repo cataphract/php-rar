16db0dc83dbf42360c66b916f90b378c919c0e57
cataphract <cataphract@c90b9560-bf6c-de11-be94-00142212c4b1>
2011-06-12 02:08:51 +0000

    Added unrar 4.0.7
    git-svn-id: https://svn.php.net/repository/pecl/rar/trunk@312077 c90b9560-bf6c-de11-be94-00142212c4b1

--- unrar/version.hpp
+++ unrar/version.hpp
@@ -1,7 +1,7 @@
 #define RARVER_MAJOR     4
 #define RARVER_MINOR     0
-#define RARVER_BETA      5
+#define RARVER_BETA      0
 #define RARVER_PATCH     1
-#define RARVER_DAY      24
-#define RARVER_MONTH     1
+#define RARVER_DAY       12
+#define RARVER_MONTH     6
 #define RARVER_YEAR   2011
