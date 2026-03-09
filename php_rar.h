2ef087bd9ea4caa3ac163b35e314be7be6d5c527
Gustavo Lopes <mail@geleia.net>
2026-03-09 10:34:59 +0000

    Version validation in CI

--- php_rar.h
+++ php_rar.h
@@ -51,7 +51,7 @@
 extern zend_module_entry rar_module_entry;
 #define phpext_rar_ptr &rar_module_entry
 
-#define PHP_RAR_VERSION "4.3.0"
+#define PHP_RAR_VERSION "4.3.1-dev"
 
 #ifdef PHP_WIN32
 #define PHP_RAR_API __declspec(dllexport)
